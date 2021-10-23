09daea56b257fc8a8065aa7be81c763f1f124585
tabz1234 <disabilitylord1234@protonmail.com>
2021-10-24 02:57:01 +0300

    adopt error handling for last terminal color handling changes,also those now is not instant abort

--- src/PassmanDB.cpp
+++ src/PassmanDB.cpp
@@ -1,6 +1,6 @@
 #include "PassmanDB.hpp"
 #include "SQliteDB.hpp"
-#include "TerminalColor.hpp"
+#include "TerminalUtils.hpp"
 #include "createFile.hpp"
 #include "sqlite_return.hpp"
 
@@ -13,12 +13,17 @@
 PassmanDB::PassmanDB(const std::filesystem::path& db_path) noexcept
   : db_{ db_path }
 {
-    if (std::filesystem::file_size(db_path) == 0)
-        db_.execute("CREATE TABLE PASSWORD( "
-                    "ID VARCHAR PRIMARY KEY NOT NULL, "
-                    "STR VARCHAR NOT NULL, "
-                    "LAST_ACCES BIGINT, "
-                    "CREATED BIGINT);");
+    if (std::filesystem::file_size(db_path) == 0) [[unlikely]] {
+        const auto sql_data = db_.execute("CREATE TABLE PASSWORD( "
+                                          "ID VARCHAR PRIMARY KEY NOT NULL, "
+                                          "STR VARCHAR NOT NULL, "
+                                          "LAST_ACCES BIGINT, "
+                                          "CREATED BIGINT);");
+
+        if (sql_data.rc_) {
+            ColorfulText<FG{ 255, 0, 0 }>("Cant create sql table \n");
+        }
+    }
 }
 
 Password
@@ -27,9 +32,8 @@ PassmanDB::get(const std::string& pass_id) noexcept
     const auto sql_data =
       db_.execute("SELECT * from PASSWORD where ID = '" + pass_id + "'");
     if (sql_data.rc_) {
-        std::cout << FGColor{ 255, 0, 0 } << "cant get password with id :\""
-                  << pass_id << "\"\n\n"
-                  << FGColor{ 255, 255, 255 };
+        ColorfulText<FG{ 255, 0, 0 }>("Cant get password with id :" + pass_id +
+                                      "\n");
     }
 
     const Password temp(
@@ -49,9 +53,8 @@ void
 PassmanDB::add(const Password& password) noexcept
 {
 
-    const auto sys_clock_time = std::chrono::system_clock::now();
-    const auto since_epoch =
-      std::to_string(sys_clock_time.time_since_epoch().count());
+    const auto since_epoch = std::to_string(
+      std::chrono::system_clock::now().time_since_epoch().count());
 
     const auto sql_data = db_.execute("INSERT INTO PASSWORD (ID,STR,CREATED) "
                                       "VALUES ('" +
@@ -59,9 +62,7 @@ PassmanDB::add(const Password& password) noexcept
                                       "'," + since_epoch + ");");
 
     if (sql_data.rc_) {
-        std::cout << FGColor{ 255, 0, 0 }
-                  << "cant add to database password with id :\"" << password.id_
-                  << "\"\n\n"
-                  << FGColor{ 255, 255, 255 };
+        ColorfulText<FG{ 255, 0, 0 }>(
+          "Cant insert into database password with id :" + password.id_ + "\n");
     }
 }
