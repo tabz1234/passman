4a6ee7f5e127bcac9e4d9211b03a49c665eece4b
tabz1234 <disabilitylord1234@protonmail.com>
2021-10-26 20:25:33 +0300

    extend default config

--- resource/passman_conf.lua
+++ resource/passman_conf.lua
@@ -1,15 +1,28 @@
---This is default configuration file for passman
+--This is default configuration file for passman.
+--Passman will execute this file and then get specific variable names for internal use.
+--All variables that affect passman behavior is initialized here by default.
+--If you don't want to use this config, run "passman --no-config" and pass your settings through command line argumnets(see "passman --help")
 
-DEBUG = true
+--All static data sush as database or known_id's file stored here.
+--Override with --appdatadir=
+local homepath = os.getenv("HOME")
+AppDataDir = homepath.."/.local/share/passman"
 
-LUA_PREFIX = "[LUA]"
-function Lprint(val) 
-print(LUA_PREFIX..val);
+--Override with --lenght=
+PasswordLenght = 30
+
+--Override with --no-encryption.
+UseEncryption = true
+
+--Set UseDefaultMasterPassword to true if you lazy and don't want to always type master password.
+
+if(UseEncryption) then
+UseDefaultMasterPassword = false
+   if(UseDefaultMasterPassword) then
+	MasterPassword = "ChangeMe"
+   end
 end
 
-AppDataDir = "/usr/lib/passman"
-RandomAsciiLenght = 30
-
-if DEBUG == true then Lprint(" Using appdata path :"..AppDataDir) end
-
-
+--Display only raw result and nothing else?
+--Override with --quiet or -q
+Quiet = false
