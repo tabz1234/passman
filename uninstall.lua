af49c9a4e1d89d8ef62fd9d612164ba5d70b1fb9
tabz1234 <disabilitylord1234@protonmail.com>
2021-10-29 22:46:59 +0300

    add uninstall script

---
+++ uninstall.lua
@@ -0,0 +1,12 @@
+#!/bin/lua
+
+ExecName = "passman"
+RelType = "release"
+Nproc = 8
+
+function Print_finished()
+print("\n!!! UNINSTALL SCRIPT FINISHED !!!")
+end
+
+os.execute("sudo rm -r /usr/share/bash-completion/completions/passman ~/.local/share/passman ~/.config/passman /usr/share/fish/completions/passman.fish  /bin/passman")
+Print_finished()
